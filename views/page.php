3acb8323b33a6663756c221d89ab08073dd7dce8
iccee0 <66622055+RoeHH@users.noreply.github.com>
2020-11-20 10:30:36 +0100

    sidebar out of page

--- views/page.php
+++ views/page.php
@@ -29,19 +29,7 @@
 
 
 <?php include '../private/sidebar.php';?>
-  <div class="rightcolumn">
-    <div class="card">
-      <h2>About Me</h2>
-      <div class="fakeimg" style="height:100px;">Image</div>
-      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-    </div>
-    <div class="card">
-      <h3>Popular Post</h3>
-      <div class="fakeimg">Image</div><br>
-      <div class="fakeimg">Image</div><br>
-      <div class="fakeimg">Image</div>
-    </div>
-  </div>
+
 
 </div>
 
