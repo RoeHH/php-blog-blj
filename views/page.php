c660eb5a57b58f03187615012db2728225646bb3
iccee0 <66622055+RoeHH@users.noreply.github.com>
2020-11-20 10:51:59 +0100

    t

--- views/page.php
+++ views/page.php
@@ -6,7 +6,9 @@
     </head>
 <body>
         <?php include '../private/header.php';?>
+
         <?php include '../private/row.php';?>
+        
         <?php include '../private/footer.php';?>
     </body>
 </html>
