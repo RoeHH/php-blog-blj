23bda6163bddf1e197184b4d90efcb613706ec78
iccee0 <66622055+RoeHH@users.noreply.github.com>
2020-11-20 10:07:00 +0100

    again

--- views/page.php
+++ views/page.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="style" href="public/stylesheets/style.css">
-</head>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="../public/stylesheets/style.css">
+    </head>
 <body>
 
 <div class="header">
