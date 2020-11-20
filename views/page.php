ede38f581190e01fb93504501763bd572c7e5abc
iccee0 <66622055+RoeHH@users.noreply.github.com>
2020-11-20 11:02:29 +0100

    background

--- views/page.php
+++ views/page.php
@@ -3,12 +3,14 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="../public/stylesheets/style.css">
+        <link rel="stylesheet" type="text/css" href="../public/stylesheets/background.css">
+
     </head>
 <body>
         <?php include '../private/header.php';?>
 
         <?php include '../private/row.php';?>
-        
+
         <?php include '../private/footer.php';?>
     </body>
 </html>
