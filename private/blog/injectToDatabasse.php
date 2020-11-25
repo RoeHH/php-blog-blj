ec0d41e5712d16c9d8c81177dba157ffd6ffab99
iccee0 <github.r@12i.ch>
2020-11-25 10:11:27 +0100

    lol fixed

--- private/blog/injectToDatabasse.php
+++ private/blog/injectToDatabasse.php
@@ -20,8 +20,18 @@
     foreach($form as $result) {
         echo $result, "<br>";
     }
-    $db = new PDO('mysql:host=localhost;SHOW ERRORS;dbname=blogdb', 'root', '');
-    $stmt = $db->prepare('INSERT INTO posts (created_by, post_title, post_text, created_at)
+    try {
+        $user = 'root';
+        $password = '';
+        $pdo = new PDO('mysql:host=localhost;dbname=blogdb', $user, $password, [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    ]);
+}
+catch (PDOException $e) {
+        die('Keine Verbindung zur Datenbank möglich: ' . $e->getMessage());
+}
+    $stmt = $pdo->prepare('INSERT INTO posts (created_by, post_title, post_text, created_at)
                                     VALUES (:created_by, :post_title, :post_text, now())');
     $stmt->execute([":created_by" => "$form[nickname]", ":post_title" => "$form[title]", ":post_text" => "$form[text]"]);
 ?>
