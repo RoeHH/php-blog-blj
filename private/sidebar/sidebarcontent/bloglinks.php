c294d8801d3508242f7829e46ec918187514eba5
iccee0 <github.r@12i.ch>
2020-11-25 13:31:02 +0100

    links in place

--- private/sidebar/sidebarcontent/bloglinks.php
+++ private/sidebar/sidebarcontent/bloglinks.php
@@ -1,5 +1,21 @@
+<?php
+    $dbuser = "d041e_listuder";
+
+    $dbpass = "12345_Db!!!";
+
+    $pdo = new PDO("mysql:host=mysql2.webland.ch;dbname=d041e_listuder", $dbuser, $dbpass, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    ]);
+
+    $sqlQuery = $pdo->query("SELECT * FROM `blog_url`");
+    $urls = $sqlQuery->fetchAll();
+?>
 <div class="card">
-    <h2>About Me</h2>
-    <div class="fakeimg" style="height:100px;">Image</div>
-    <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+    <h3>Other amazing Blogs</h3>
+    <?php
+    foreach ($urls as $key => $value) {
+        echo "<a href=\"$value[2]\">$value[0]. $value[1]</a><br>";
+    }
+    ?>
 </div>
